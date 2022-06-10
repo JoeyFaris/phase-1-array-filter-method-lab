18977792e427c643cce367162eb3239bd0231aca
Joey Faris <joeyfaris1@yahoo.com>
2022-06-09 21:08:10 -0700

    commit tree 4db71abc4148f4eb7f1e5dea991a28f7a7e94b3d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name) {
+    return array.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+
+  function fuzzyMatch() {
+    
+  }
